d9c4056febc15aa65739bb8ead642bd1be201000
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-09-08 20:33:27 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
 let gameboard = (() => {
   let array = new Array(9);
   array.fill(undefined);
+  let gameover = false;
   const clear_board = () => {
     array.fill(undefined);
   };
   const checkwin = (xoro) => {
-    let temparr = [];
+    let _temparr = [];
     for (let i = 0; i < 9; i++) {
       if (array[i] === xoro) {
-        temparr.push(i);
-        console.log(temparr);
+        _temparr.push(i);
         if (
-          (temparr.includes(0) && temparr.includes(1) && temparr.includes(2)) ||
-          (temparr.includes(3) && temparr.includes(4) && temparr.includes(5)) ||
-          (temparr.includes(6) && temparr.includes(7) && temparr.includes(8)) ||
-          (temparr.includes(0) && temparr.includes(3) && temparr.includes(6)) ||
-          (temparr.includes(1) && temparr.includes(4) && temparr.includes(7)) ||
-          (temparr.includes(2) && temparr.includes(5) && temparr.includes(8)) ||
-          (temparr.includes(0) && temparr.includes(4) && temparr.includes(8)) ||
-          (temparr.includes(2) && temparr.includes(4) && temparr.includes(6))
+          (_temparr.includes(0) && _temparr.includes(1) &&
+            _temparr.includes(2)) ||
+          (_temparr.includes(3) && _temparr.includes(4) &&
+            _temparr.includes(5)) ||
+          (_temparr.includes(6) && _temparr.includes(7) &&
+            _temparr.includes(8)) ||
+          (_temparr.includes(0) && _temparr.includes(3) &&
+            _temparr.includes(6)) ||
+          (_temparr.includes(1) && _temparr.includes(4) &&
+            _temparr.includes(7)) ||
+          (_temparr.includes(2) && _temparr.includes(5) &&
+            _temparr.includes(8)) ||
+          (_temparr.includes(0) && _temparr.includes(4) &&
+            _temparr.includes(8)) ||
+          (_temparr.includes(2) && _temparr.includes(4) && _temparr.includes(6))
         ) {
           console.log(xoro + " wins");
+          gameover = true;
           break;
         }
       }
     }
   };
-  return { array, clear_board, checkwin };
+  return { array, gameover, clear_board, checkwin };
 })();
 let player = (xoro) => {
   const get_xoro = () => xoro;
   const playturn = (index) => {
-    gameboard.array[index] = xoro;
-    gameboard.checkwin(xoro);
+    console.log(gameboard.gameover);
+    if (
+      (typeof gameboard.array[index] === "undefined") && (!gameboard.gameover)
+    ) {
+      gameboard.array[index] = xoro;
+      gameboard.checkwin(xoro);
+    }
   };
   return { get_xoro, playturn };
 };
@@ -40,12 +53,12 @@ let player = (xoro) => {
 let player1 = player("x");
 let comp = player("o");
 player1.playturn(2);
+console.log(gameboard.gameover);
 player1.playturn(0);
 player1.playturn(1);
+console.log(gameboard.gameover);
 comp.playturn(7);
 comp.playturn(8);
 comp.playturn(6);
 console.log(gameboard.array);
-if (gameboard.array[2]) {
-  console.log("yay");
-}
+console.log(gameboard.gameover);
